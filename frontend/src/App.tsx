e5a2cb46c533f30c13f489ee6d79abf0aeb50775
Eliab <147437693+EliabW@users.noreply.github.com>
2026-01-19 19:25:59 -0600

    should now have a functional resuable header and an exercise library page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,19 +4,23 @@ import Register from "./pages/Register.jsx";
 import ProtectedRoute from "./components/features/auth/ProtectedRoute.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
 import RedirectIfAuth from "./components/features/auth/RedirectIfAuth.jsx";
+import About from "./pages/About.tsx";
+import ExerciseLibrary from "./pages/ExerciseLibrary.tsx";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="/" element={<Navigate to="/about" replace />} />
       {/* Auth routes (shouldn't be visible if logged in) */}
       <Route element={<RedirectIfAuth />}>
+        <Route path="/about" element={<About />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Route>
       {/* Protected routes */}
       <Route element={<ProtectedRoute />}>
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/library" element={<ExerciseLibrary />} />
       </Route>
     </Routes>
   );
--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,8 +1,10 @@
 import { DarkModeToggle } from "@/components/features/dashboard/DarkModeToggle";
+import Header from "@/components/layout/Header";
 
 function Dashboard() {
   return (
     <>
+      <Header></Header>
       <h1 className="text-3xl font-bold underline">Dashboard</h1>
       <DarkModeToggle></DarkModeToggle>
     </>
